c1b920d0ffb01bc191f142b1191a55d5403e4687
igdev <dorultanianos@gmail.com>
2025-11-11 12:00:35 +0200

    fix: missing re-exported TokenList type from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 import tokenListSchema from "../schemas/tokenlist.schema.json";
 import tokenListRulesSchema from "../schemas/tokenlist-rules.schema.json";
+export type {TokenList} from "@uniswap/token-lists";
 
 export {tokenListSchema, tokenListRulesSchema};
 
