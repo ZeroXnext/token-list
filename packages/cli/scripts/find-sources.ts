47f55ef17985fed7fead7eed54e4821b2cb67ec4
igdev <dorultanianos@gmail.com>
2025-11-11 16:45:47 +0200

    fix(cli): handle date-time formats

--- packages/cli/scripts/find-sources.ts
+++ packages/cli/scripts/find-sources.ts
@@ -1,16 +1,23 @@
 import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
 import {LIST_SOURCES} from '../constants';
+import addFormats from 'ajv-formats';
 import Ajv from 'ajv';
 
 
 const ajv = new Ajv();
 const validator = ajv.addSchema(tokenListSchema);
+addFormats(ajv);
 
 async function main(sources: string[]) {
   return Promise.all(sources.map<Promise<TokenList>>(async (item) => {
     const res = await fetch(LIST_SOURCES[item]);
-    const data = await res.json() as TokenList;
-    if (!validator.validate(tokenListSchema, data)) {
+    let data = await res.json();
+    const result = validator.validate(tokenListSchema, data);
+    const errorsPath = validator.errors?.map(item => item.instancePath) ?? [];
+    if (errorsPath.includes('#/properties/tokens/maxItems') && errorsPath.length === 1) {
+      return data;
+    }
+    if (!result) {
       throw new Error(`The list ${item} from ${LIST_SOURCES[item]} is invalid.`);
     }
     return data;
