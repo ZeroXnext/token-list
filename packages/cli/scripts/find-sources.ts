29303d6d36a759acbff2056076a5fd5551b2924f
igdev <dorultanianos@gmail.com>
2025-11-12 10:47:22 +0200

    feat(cli): implement defaultTokenListName and defaultVersion as optional args to find-sources script

--- packages/cli/scripts/find-sources.ts
+++ packages/cli/scripts/find-sources.ts
@@ -1,5 +1,5 @@
 import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
-import {DEFAULT_TOKEN_LIST_NAME, LIST_SOURCES} from '../constants';
+import {DEFAULT_LIST_VERSION, DEFAULT_TOKEN_LIST_NAME, LIST_SOURCES} from '../constants';
 import addFormats from 'ajv-formats';
 import Ajv from 'ajv';
 import {partitionTokenList} from '../utils';
@@ -10,12 +10,12 @@ const validator = ajv.addSchema(tokenListSchema);
 addFormats(ajv);
 
 
-async function main(sources: string[]): Promise<TokenList[]> {
+async function main(sources: string[], defaultTokenListName = DEFAULT_TOKEN_LIST_NAME, defaultVersion = DEFAULT_LIST_VERSION): Promise<TokenList[]> {
   return Promise.all(sources.flatMap<Promise<TokenList[]>>(async (item) => {
     const res = await fetch(LIST_SOURCES[item]);
     let data = await res.json();
 
-    const maybePartitionedLists = (data as TokenList).tokens.length > tokenListSchema.properties.tokens.maxItems ? partitionTokenList(data) : [data as MutableTokenList];
+    const maybePartitionedLists = (data as TokenList).tokens.length > tokenListSchema.properties.tokens.maxItems ? partitionTokenList(data, defaultVersion, undefined, defaultTokenListName) : [data as MutableTokenList];
 
     for (let list of maybePartitionedLists) {
 
@@ -24,7 +24,7 @@ async function main(sources: string[]): Promise<TokenList[]> {
 
       if (schemaPaths.includes("#/properties/name/maxLength")) {
 
-        list.name = DEFAULT_TOKEN_LIST_NAME;
+        list.name = defaultTokenListName;
         result = validator.validate(tokenListSchema, data);
       }
       if (!result) {
