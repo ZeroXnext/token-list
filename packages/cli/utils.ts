1e036b26b334709ae121afff72ab666ca5894e12
igdev <dorultanianos@gmail.com>
2025-11-12 09:06:42 +0200

    feat(cli): impl token list partition util

--- packages/cli/utils.ts
+++ packages/cli/utils.ts
@@ -1,4 +1,31 @@
-import {TokenList} from "@tokenlist-builder/core";
+import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
+
+export function partitionArray<T>(array: T[], maxItems:number): T[][] {
+  if (maxItems <= 0) throw new Error("maxItems must be greater than 0");
+
+  const result: T[][] = [];
+  for (let i = 0; i < array.length; i += maxItems) {
+    result.push(array.slice(i, i + maxItems));
+  }
+  return result;
+}
+
+export function partitionTokenList(tokenList: TokenList): TokenList[] {
+  // Copy metadata from the list and the partitioned tokens
+  const tokens = partitionArray(tokenList.tokens, tokenListSchema.properties.tokens.maxItems);
+  let tokenLists: TokenList[] = [];
+  let i = 0;
+  for (const _tokens of tokens) {
+    i++;
+    tokenLists.push({
+      ...tokenList,
+      name: `${tokenList.name}${i}`, // use unique name because this will be the filename
+      tokens: _tokens
+    })
+  }
+
+  return tokenLists;
+}
 
 export function createDefaultList(chainName: string): TokenList {
   return {
