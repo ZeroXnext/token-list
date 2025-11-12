9905ce169880cc73f67a46f2ba7dba5edfe70007
igdev <dorultanianos@gmail.com>
2025-11-12 10:42:28 +0200

    feat(cli): implement support for partitioning and invalid name edge cases

--- packages/cli/scripts/find-sources.ts
+++ packages/cli/scripts/find-sources.ts
@@ -1,28 +1,40 @@
 import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
-import {LIST_SOURCES} from '../constants';
+import {DEFAULT_TOKEN_LIST_NAME, LIST_SOURCES} from '../constants';
 import addFormats from 'ajv-formats';
 import Ajv from 'ajv';
-
+import {partitionTokenList} from '../utils';
+import {MutableTokenList} from '../types';
 
 const ajv = new Ajv();
 const validator = ajv.addSchema(tokenListSchema);
 addFormats(ajv);
 
-async function main(sources: string[]) {
-  return Promise.all(sources.map<Promise<TokenList>>(async (item) => {
+
+async function main(sources: string[]): Promise<TokenList[]> {
+  return Promise.all(sources.flatMap<Promise<TokenList[]>>(async (item) => {
     const res = await fetch(LIST_SOURCES[item]);
     let data = await res.json();
-    const result = validator.validate(tokenListSchema, data);
-    // Ignore tokens/maxItems, we should produce two separate lists in this case
-    const schemaPaths = validator.errors?.map(item => item.schemaPath) ?? [];
-    if (schemaPaths.includes('#/properties/tokens/maxItems') && schemaPaths.length === 1) {
-      return data;
+
+    const maybePartitionedLists = (data as TokenList).tokens.length > tokenListSchema.properties.tokens.maxItems ? partitionTokenList(data) : [data as MutableTokenList];
+
+    for (let list of maybePartitionedLists) {
+
+      let result = validator.validate(tokenListSchema, list);
+      const schemaPaths = validator.errors?.map(item => item.schemaPath) ?? [];
+
+      if (schemaPaths.includes("#/properties/name/maxLength")) {
+
+        list.name = DEFAULT_TOKEN_LIST_NAME;
+        result = validator.validate(tokenListSchema, data);
+      }
+      if (!result) {
+        throw new Error(validator.errors?.join(", "));
+      }
+
     }
-    if (!result) {
-      throw new Error(`The list ${item} from ${LIST_SOURCES[item]} is invalid.`);
-    }
-    return data;
-  }));
+
+    return maybePartitionedLists;
+  }).flat() as never as TokenList[]);
 }
 
 export default main;
