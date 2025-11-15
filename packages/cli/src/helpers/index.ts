367fac3b795ade7cf1a3d06664e165a2479d5e74
igdev <dorultanianos@gmail.com>
2025-11-15 14:53:47 +0200

    refactor(cli): remove unused unique path & refs to it

--- packages/cli/src/helpers/index.ts
+++ packages/cli/src/helpers/index.ts
@@ -2,6 +2,5 @@ import classify from './classify';
 import load from './load-flat-local';
 import output from './output';
 import initializeTokenList from './initialize-token-list';
-import getUniqueFilePath from './unique-path';
 import bump from './bump';
-export {classify, load, output, initializeTokenList, getUniqueFilePath, bump};
+export {classify, load, output, initializeTokenList, bump};
