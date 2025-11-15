91b1b5f72474cc6a8b70467671e682f41554746e
igdev <dorultanianos@gmail.com>
2025-11-15 13:39:38 +0200

    feat(cli): implement a better alternative to old classification algo

--- packages/cli/src/helpers/classify.ts
+++ packages/cli/src/helpers/classify.ts
@@ -1,14 +1,73 @@
 import {TokenList, tokenListSchema} from "@tokenlist-builder/core";
-import {TokenListsMap} from '@types';
+import {ListPath, Mutable, SeenKey, TokenListsMap} from '@types';
 import {CHAINS_MAPPING} from '@constants';
 import {initializeTokenList} from '@helpers';
 import {slugify} from '@utils';
+
+export function classify(tokenList: TokenList, supportedNetworks: string[], supportedChains: string[], rootDir: string, seen: Set<SeenKey>, offset = -1): Map<string, TokenList> {
+  const mapping = new Map<ListPath, TokenList>([]);
+  for (let i = Math.max(offset, 0); i < tokenList.tokens.length; i++) {
+
+    const token = tokenList.tokens[i];
+    // 1. Ignore if token doesn't have a chain id
+    if (!token.chainId || !token.address) {
+      continue;
+    }
+
+    const {name, type} = CHAINS_MAPPING[token.chainId];
+
+    // 2. Check if this token has been seen
+    if (seen.has(`${type}:${name}:${token.address}`)) {
+      continue;
+    }
+
+    // 3. Check if network or chains are supported
+    if (!supportedNetworks.includes(type) || !supportedChains.includes(name)) {
+      continue;
+    }
+
+    // 4. Initialize listPath
+    let listPath: ListPath = `${rootDir}/${type}/${name}/${slugify(name)}.json`;
+
+    // 5. Check if there is an offset available, if so modify token list name and listPath
+    let tokenListName: string = tokenList.name;
+    if (offset > -1) {
+      tokenListName = tokenList.name + ` ${offset}`
+      listPath = `${rootDir}/${type}/${name}/${slugify(name)}-${offset}.json`;
+    }
+
+    let list: Mutable<TokenList> | undefined = mapping.get(listPath);
+
+    // 3. Check if there is an existing list, otherwise initialize empty tokens
+    if (!list) {
+      mapping.set(listPath, initializeTokenList({
+        name: tokenListName,
+        tags: tokenList.tags,
+        tokens: [],
+        keywords: tokenList.keywords,
+        logoURI: tokenList.logoURI
+      }));
+      list = mapping.get(listPath);
+    }
+
+    // 4. Check if the list has reached the maximum tokens, if so write another list
+    const maxTokensPerList = tokenListSchema.properties.tokens.maxItems;
+    if (list?.tokens.length ?? 0 > maxTokensPerList) {
+      offset += 1;
+      classify(tokenList, supportedNetworks, supportedChains, rootDir, seen, offset);
+    }
+
+    // 5. Push token its path
+    list?.tokens.push(token);
+  }
+
+  return mapping;
+}
 
 function main(tokenLists: TokenList[], supportedChains: string[], supportedNetworks: string[], initialMap: TokenListsMap = new Map(), initialSeen = new Set<string>(), verbose: boolean): TokenListsMap {
   // [network_type]/[chain_name]/[file_key]/[actual_list]
   const mappedTokenList: TokenListsMap = initialMap;
   const seen = initialSeen;
-
 
   for (const tokenList of tokenLists) {
     for (const token of tokenList.tokens) {
--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -5,6 +5,7 @@ export interface Chain {
   name: string;
   type: string;
 }
+
 // [network_type]/[chain_name]/[file_key]/[actual_list]
 export type TokenListsMap = Map<string, Map<string, Map<string, TokenList>>>;
 
@@ -16,3 +17,10 @@ export type MutableTokenList = Mutable<TokenList>;
 
 
 export type Entry = typeof entry;
+
+
+// <root>/<network_type>/<chain_name>/<list-name>.json
+export type ListPath = `${string}/${string}/${string}/${string}.json`;
+
+// <network_type>:<chain_id>:<token_address>
+export type SeenKey = `${string}:${string}:${string}`
