e4a7b3cda974505507399e925de79d9facad068c
igdev <dorultanianos@gmail.com>
2025-11-15 09:09:14 +0200

    feat(cli): implement list versioning

--- packages/cli/src/helpers/diff-token-lists.ts
+++ packages/cli/src/helpers/diff-token-lists.ts
@@ -1,4 +1,6 @@
 import {TokenList} from '@tokenlist-builder/core';
+import {Mutable} from '@types';
+import {timestamp} from '@utils';
 
 interface TokenListChangeLog {
   added: TokenList['tokens'],
@@ -12,24 +14,27 @@ interface TokenListChangeLog {
  * - Increment patch version when tokens already on the list have minor details changed (name, symbol, logo URL, decimals)
  * - Changing a token address or chain ID is considered both a remove and an add, and should be a major version update.
  */
-export function diffTokenLists(oldList: TokenList, newList: TokenList): [TokenList['version'], TokenListChangeLog] {
-
+export function diffTokenLists(oldList: TokenList, newList: Mutable<TokenList>) {
   const added: TokenList['tokens'] = [];
   const removed: TokenList['tokens'] = [];
   const modified: TokenList['tokens'] = [];
-  const version: TokenList['version'] = oldList.version;
+  const version: Mutable<TokenList['version']> = oldList.version;
+  let increment: keyof TokenList['version'] | undefined;
 
   for (let token of newList.tokens) {
     const tokenInfo = oldList.tokens.find(oldToken => oldToken.address !== token.address);
     if (tokenInfo) {
+      // New Token has been added
       added.push(token);
+      increment = "minor";
     }
   }
 
   for (let token of oldList.tokens) {
     const tokenInfo = newList.tokens.find(tokenInfo => tokenInfo.address === token.address);
-
     if (!tokenInfo) {
+      // Token has been removed
+      increment = "major";
       removed.push(token);
     }
 
@@ -40,8 +45,13 @@ export function diffTokenLists(oldList: TokenList, newList: TokenList): [TokenLi
         tokenInfo.logoURI !== token.logoURI ||
         JSON.stringify(tokenInfo.tags) !== JSON.stringify(token.tags))) {
       modified.push(token);
+      increment = "patch";
+    }
+    // increment version
+    if (increment) {
+      version[increment] += 1;
+      newList.version = version;
+      newList.timestamp = timestamp();
     }
   }
-
-  return [version, {added, modified, removed}];
 }
