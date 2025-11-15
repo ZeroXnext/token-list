b507f9a4d6ff0188aa69a38dc23bef1b204c6c68
igdev <dorultanianos@gmail.com>
2025-11-15 14:47:36 +0200

    refactor(cli)!: remove unnecessary "normalize" helper

--- packages/cli/src/helpers/index.ts
+++ packages/cli/src/helpers/index.ts
@@ -1,9 +1,8 @@
 import classify from './classify';
-import load from './load';
+import load from './load-flat-local';
 import resolve from './resolve';
 import output from './output';
 import initializeTokenList from './initialize-token-list';
 import getUniqueFilePath from './unique-path';
-import {normalizeTokenList} from './normalize';
-
-export {classify, load, resolve, output, initializeTokenList, getUniqueFilePath, normalizeTokenList};
+import bump from './bump';
+export {classify, load, resolve, output, initializeTokenList, getUniqueFilePath, bump};
