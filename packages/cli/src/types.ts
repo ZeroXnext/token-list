6b2630a5734c6112087f67c08a4212e5796f9595
igdev <dorultanianos@gmail.com>
2025-11-15 17:15:18 +0200

    refactor(cli): remove unused types

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -1,4 +1,3 @@
-import {TokenList} from '@tokenlist-builder/core';
 import {entry} from './index';
 
 export interface Chain {
@@ -6,18 +5,12 @@ export interface Chain {
   type: string;
 }
 
-// [network_type]/[chain_name]/[file_key]/[actual_list]
-export type TokenListsMap = Map<string, Map<string, Map<string, TokenList>>>;
-
 export type Mutable<T> = {
   -readonly [P in keyof T]: T[P];
 };
 
-export type MutableTokenList = Mutable<TokenList>;
-
 
 export type Entry = typeof entry;
-
 
 // <root>/<network_type>/<chain_name>/<list-name>.json
 export type ListPath = `${string}/${string}/${string}/${string}.json`;
