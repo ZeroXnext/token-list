cc67c8f4f0621fc175d0bfadaad1ffdc99a7b592
igdev <dorultanianos@gmail.com>
2025-11-16 09:01:58 +0200

    refactor(cli): subtract the paths read fn into its own helper

---
+++ packages/cli/src/helpers/get-paths.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export default function getPaths(outputDir: string) {
+  return fs.readdirSync(outputDir, {recursive: true}).map(toString).filter((p) => {
+    try {
+      return fs.statSync(path.join(outputDir, p.toString())).isFile();
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  });
+}
--- packages/cli/src/helpers/load.ts
+++ packages/cli/src/helpers/load.ts
@@ -3,16 +3,10 @@ import {TokenList} from '@tokenlist-builder/core';
 import path from 'node:path';
 import {SeenKey} from '@types';
 import {CHAINS_MAPPING} from '@constants';
+import getPaths from './get-paths';
 
 export default function load(outputDir: string): [Map<string, TokenList>, Set<SeenKey>] {
-  const paths = fs.readdirSync(outputDir, {recursive: true}).filter((p) => {
-    try {
-      return fs.statSync(path.join(outputDir, p.toString())).isFile();
-    } catch (e) {
-      console.error(e);
-      return false;
-    }
-  });
+  const paths = getPaths(outputDir);
   const seen = new Set<SeenKey>();
   return [new Map<string, TokenList>(paths.map(key => {
     const list = JSON.parse(fs.readFileSync(path.join(outputDir, key.toString()), 'utf-8')) as TokenList;
