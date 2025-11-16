c7a2ab11cd527adab0fbc7769a03a200f25603a4
igdev <dorultanianos@gmail.com>
2025-11-16 18:56:13 +0200

    feat(core): export types and index json schema

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
 import tokenListRulesSchema from "../schemas/tokenlist-rules.schema.json";
+import tokenListIndexSchema from "../schemas/tokenlist-index.schema.json";
 
 export type {TokenList} from "@uniswap/token-lists";
+export * from "./types";
 export {schema as tokenListSchema} from "@uniswap/token-lists";
-
-export {tokenListRulesSchema};
-
+export {tokenListRulesSchema, tokenListIndexSchema};
