8a1325d44a8915603838c54af2f944d0e3ee871b
igdev <dorultanianos@gmail.com>
2025-11-16 12:16:08 +0200

    refactor(cli): change map key type to explicit ListPath type alias

--- packages/cli/src/helpers/load.ts
+++ packages/cli/src/helpers/load.ts
@@ -1,19 +1,20 @@
 import fs from 'node:fs';
 import {TokenList} from '@tokenlist-builder/core';
 import path from 'node:path';
-import {SeenKey} from '@types';
+import {ListPath, SeenKey} from '@types';
 import {CHAINS_MAPPING} from '@constants';
 import getPaths from './get-paths';
 
-export default function load(outputDir: string): [Map<string, TokenList>, Set<SeenKey>] {
+export default function load(outputDir: string): [Map<ListPath, TokenList>, Set<SeenKey>] {
   const paths = getPaths(outputDir);
   const seen = new Set<SeenKey>();
-  return [new Map<string, TokenList>(paths.map(key => {
-    const list = JSON.parse(fs.readFileSync(path.join(outputDir, key.toString()), 'utf-8')) as TokenList;
+  return [new Map<ListPath, TokenList>(paths.map(key => {
+    const listKey = path.join(outputDir, key.toString()) as ListPath;
+    const list = JSON.parse(fs.readFileSync(listKey, 'utf-8')) as TokenList;
     for (let {address, chainId} of list.tokens) {
       const {name, type} = CHAINS_MAPPING[chainId];
       seen.add(`${type}:${name}:${address}`);
     }
-    return [path.join(outputDir, key.toString()), list];
+    return [path.join(outputDir, key.toString()) as ListPath, list as TokenList];
   })), seen];
 }
