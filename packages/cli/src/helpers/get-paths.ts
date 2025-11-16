e35b866ddcaf3f24a7ab003c37f6daf5a88a5fb2
igdev <dorultanianos@gmail.com>
2025-11-16 09:13:35 +0200

    fix(cli): paths were not mapped correctly

--- packages/cli/src/helpers/get-paths.ts
+++ packages/cli/src/helpers/get-paths.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 export default function getPaths(outputDir: string) {
-  return fs.readdirSync(outputDir, {recursive: true}).map(toString).filter((p) => {
+  return fs.readdirSync(outputDir, {recursive: true}).map(item => item.toString()).filter((p) => {
     try {
       return fs.statSync(path.join(outputDir, p.toString())).isFile();
     } catch (e) {
