ef34d9acc05cc837cc8db352f7a088db0d6cb814
igdev <dorultanianos@gmail.com>
2025-11-18 18:54:01 +0200

    feat(cli): make indexFileName excluded from list paths (#1)

--- packages/cli/src/helpers/get-paths.ts
+++ packages/cli/src/helpers/get-paths.ts
@@ -3,12 +3,13 @@ import path from 'node:path';
 
 /**
  * @param outputDir – the output directory
+ * @param indexFileName – The filename of the index
  * @returns string[]
  * @description A basic method that will retrieve the token lists paths excluding the index.
  */
-export default function getPaths(outputDir: string) {
+export default function getPaths(outputDir: string, indexFileName: string) {
   return fs.readdirSync(outputDir, {recursive: true}).map(item => item.toString()).filter((p) => {
-    if (p === "index.json") {
+    if (p === `${indexFileName}.json`) {
       return false;
     }
     try {
