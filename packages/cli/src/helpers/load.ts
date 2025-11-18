d61fa7aa224db4a29f91c7b2748f0b52e383f942
igdev <dorultanianos@gmail.com>
2025-11-18 18:56:55 +0200

    refactor(cli): make loader accept config directly

--- packages/cli/src/helpers/load.ts
+++ packages/cli/src/helpers/load.ts
@@ -1,18 +1,18 @@
 import fs from 'node:fs';
-import {Chain, ChainMappingType, ListPath, SeenKey, TokenList} from '@tokenlist-builder/core';
+import {Chain, Config, ListPath, SeenKey, TokenList} from '@tokenlist-builder/core';
 import path from 'node:path';
 import getPaths from './get-paths';
 
-export default function load(outputDir: string, chainsMapping: ChainMappingType): [Map<ListPath, TokenList>, Set<SeenKey>] {
-  const paths = getPaths(outputDir);
+export default function load(config: Config): [Map<ListPath, TokenList>, Set<SeenKey>] {
+  const paths = getPaths(config.outputDir, config.indexFileName);
   const seen = new Set<SeenKey>();
   return [new Map<ListPath, TokenList>(paths.map(key => {
-    const listKey = path.join(outputDir, key.toString()) as ListPath;
+    const listKey = path.join(config.outputDir, key.toString()) as ListPath;
     const list = JSON.parse(fs.readFileSync(listKey, 'utf-8')) as TokenList;
     for (let {address, chainId} of list.tokens) {
-      const {name, type} = chainsMapping?.get(chainId) as Chain;
+      const {name, type} = config.chainsMapping?.get(chainId) as Chain;
       seen.add(`${type}:${name}:${address}`);
     }
-    return [path.join(outputDir, key.toString()) as ListPath, list as TokenList];
+    return [path.join(config.outputDir, key.toString()) as ListPath, list as TokenList];
   })), seen];
 }
