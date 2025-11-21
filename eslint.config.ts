5f56936d5631e45d6282312c49e9f6f2e007a7b8
igdev <dorultanianos@gmail.com>
2025-11-21 14:19:12 +0200

    chore: add prettier config to eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import {defineConfig, globalIgnores} from "eslint/config";
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores([".turbo", "packages/**/node_modules", "packages/**/dist", "packages/**/.rollup.cache", "*.build.tsbuildinfo"]),
@@ -12,4 +13,5 @@ export default defineConfig([
     languageOptions: {globals: globals.browser}
   },
   tseslint.configs.recommended,
+  prettierConfig
 ]);
