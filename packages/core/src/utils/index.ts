eb8dd92df890d55a5ab38dee2db4283de241db86
igdev <dorultanianos@gmail.com>
2025-11-21 12:39:43 +0200

    feat(core): export utils from index

---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,10 @@
+import slugify from "./slugify.js"
+import timestamp from "./timestamp.js"
+
+export {
+    generateKeyPair,
+    signMessage,
+    verifyMessage
+} from "./crypto.js"
+
+export { slugify, timestamp }
